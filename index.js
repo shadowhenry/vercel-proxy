b45cec8fda4988992f05a931ea3750e8f16f776e
henry <island_1123@163.com>
2025-03-01 00:02:55 +0800

    firstcommit

---
+++ index.js
@@ -0,0 +1,18 @@
+const micro = require('micro');
+const fetch = require('node-fetch');
+
+const server = micro(async (req, res) => {
+  try {
+    const url = 'https://start.boldvoice.com/accent-guesser' + req.url;
+    const response = await fetch(url);
+    const contentType = response.headers.get('Content-Type') || '';
+    res.setHeader('Content-Type', contentType);
+    res.statusCode = response.status;
+    res.end(await response.text());
+  } catch (error) {
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
+});
+
+module.exports = server;
